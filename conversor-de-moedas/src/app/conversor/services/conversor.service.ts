37a19902662f0566f32ac0ad7bc8c4ee39faecfb
gustavo <gustavonarede1@gmail.com>
2020-03-27 00:28:25 -0300

    alteracao linha 15 e 24

--- conversor-de-moedas/src/app/conversor/services/conversor.service.ts
+++ conversor-de-moedas/src/app/conversor/services/conversor.service.ts
@@ -12,7 +12,7 @@ import{
 })
 export class ConversorService {
 
-	private readonly BASE_URL = "http://api.fixer.io/latest";
+	private readonly BASE_URL = "http://data.fixer.io/api/latest?access_key=eba7130a5b2d720ce43eb5fcddd47cc3";
   constructor(private http: HttpClient) { }
   /**
    * Realiza a chamada para a API de conversão de moedas.
@@ -21,7 +21,7 @@ export class ConversorService {
    * @return Observable<ConversaoResponse>
    */
   converter(conversao: Conversao): Observable<any> {
-  	let params = `?base=${conversao.moedaDe}&symbols=${conversao.moedaPara}`;
+  	let params = `&base=${conversao.moedaDe}&symbols=${conversao.moedaPara}`;
 
   	return this.http
 
